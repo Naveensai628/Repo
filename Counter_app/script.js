b1612eaad5a3d128ee37380f50be19711cf66f03
Naveensai628 <galinaveensai7@gmail.com>
2022-06-20 18:20:50 +0530

    Counter_app/script.js

--- Counter_app/script.js
+++ Counter_app/script.js
@@ -2,17 +2,36 @@ const count = document.querySelector(".count");
 const add = document.querySelector(".add");
 const resetCount = document.querySelector(".reset");
 const sub = document.querySelector(".subtract");
+const buttons = document.querySelector(".buttons");
+
+// buttons.addEventListener("click", (e) => {
+// if(e.target.classList.contains("add")){
+//     count.innerHTML++;
+// }
+// });
 
 add.addEventListener("click", () => {
     count.innerHTML++;
+    setColor();
 });
 
 sub.addEventListener("click", () => {
     count.innerHTML--;
+    setColor();
 });
 
 resetCount.addEventListener("click", () => {
     count.innerHTML=0;
+    setColor();
 });
 
+function setColor() {
+    if(count.innerHTML>0){
+        count.style.color="yellow";
+    } else if(count.innerHTML<0){
+        count.style.color="orangered";
+    } else {
+        count.style.color="white";
+    }
+}
 
